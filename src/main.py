fde4fb12778edd1bed1d184d0022d46012e7f6c5
DB8LE <db8le@darc.de>
2025-11-07 21:35:57 +0100

    Add config

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import logging
 
-from . import custom_logging
+from . import custom_logging, config
 
 
 def main():
     custom_logging.set_up_logging()
+
+    config_data = config.read_config()
+    custom_logging.set_logging_config(config_data)
 
     logging.info("Hello, World!")
